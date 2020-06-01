1709b52f956856ae34490326ca6606e4ad304e58
kensuke <itou_k@digicomnet.co.jp>
2020-06-01 11:55:42 +0900

    Add PlayDataFilter

---
+++ src/filter/PlayDataFilter.java
@@ -0,0 +1,63 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class PlayDataFilter
+ */
+@WebFilter("/*")
+public class PlayDataFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public PlayDataFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        // place your code here
+        String context_path = ((HttpServletRequest)request).getContextPath();
+        String servlet_path = ((HttpServletRequest)request).getServletPath();
+
+        //プレイ以外にアクセスした場合
+        if(!!servlet_path.matches("/play.*")){
+            HttpSession session = ((HttpServletRequest)request).getSession();
+
+            //sesionのplayerとdealerを削除
+            session.removeAttribute("player");
+            session.removeAttribute("dealer");
+        }
+        // pass the request along the filter chain
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+}
